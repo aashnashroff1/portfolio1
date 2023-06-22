704dea8af637525d16265b51ae1f0175a0d43316
Aashna shroff <aashnashroff@Aashnas-MacBook-Air.local>
2023-06-22 16:55:38 -0400

    A

--- containers/Greetings.tsx
+++ containers/Greetings.tsx
@@ -38,8 +38,7 @@ const Greetings = () => {
                     <Button
                       className="btn-white btn-icon mb-3 mb-sm-0 ml-1"
                       color="default"
-                      href='https://drive.google.com/file/d/1jgEouVTBkKTw5Zh6_r3zvtoaza0aTMcF/view?usp=sharing'
-                    >
+                      href='https://drive.google.com/file/d/1jgEouVTBkKTw5Zh6_r3zvtoaza0aTMcF/view?usp=sharing' target="_blank">
                       <span className="btn-inner--icon mr-1">
                         <i className="fa fa-file" />
                       </span>
