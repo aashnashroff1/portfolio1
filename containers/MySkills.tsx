f2b592d0f9eb478bec9df0be60e8553414118d11
Aashna shroff <aashnashroff@Aashnas-MacBook-Air.local>
2023-06-26 14:09:03 -0400

    26 june 7

--- containers/MySkills.tsx
+++ containers/MySkills.tsx
@@ -3,34 +3,34 @@ import React from 'react';
 const MySkills = () => {
   return (
     <div>
-      <div className="flex flex-wrap mt-8 flex flex-wrap justify-between">
-        <div className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40" style={{ opacity: 1, transform: 'none' }}>
+      <div className="flex flex-wrap mt-8 justify-between">
+        <div className="py-4 px-6 bg-gray-100 m-4 md:w-48 w-40 rounded-lg flex flex-col items-center hover:scale-125 cursor-pointer">
           <img alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg" className="w-12" />
-          <h4 className="text-md ml-4">C Language</h4>
+          <h4 className="text-md mt-2">C Language</h4>
         </div>
-        <div className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40" style={{ opacity: 1, transform: 'none' }}>
+        <div className="py-4 px-6 bg-gray-100 m-4 md:w-48 w-40 rounded-lg flex flex-col items-center hover:scale-125 cursor-pointer">
           <img alt="" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" className="w-12" />
-          <h4 className="text-md ml-4">TypeScript</h4>
+          <h4 className="text-md mt-2">TypeScript</h4>
         </div>
-        <div className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40" style={{ opacity: 1, transform: 'none' }}>
+        <div className="py-4 px-6 bg-gray-100 m-4 md:w-48 w-40 rounded-lg flex flex-col items-center hover:scale-125 cursor-pointer">
           <img alt="" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" className="w-12" />
-          <h4 className="text-md ml-4">Express</h4>
+          <h4 className="text-md mt-2">Express</h4>
         </div>
-        <div className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40" style={{ opacity: 1, transform: 'none' }}>
+        <div className="py-4 px-6 bg-gray-100 m-4 md:w-48 w-40 rounded-lg flex flex-col items-center hover:scale-125 cursor-pointer">
           <img alt="" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" className="w-12" />
-          <h4 className="text-md ml-4">NodeJS</h4>
+          <h4 className="text-md mt-2">NodeJS</h4>
         </div>
-        <div className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40" style={{ opacity: 1, transform: 'none' }}>
+        <div className="py-4 px-6 bg-gray-100 m-4 md:w-48 w-40 rounded-lg flex flex-col items-center hover:scale-125 cursor-pointer">
           <img alt="" src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" className="w-12" />
-          <h4 className="text-md ml-4">Postman</h4>
+          <h4 className="text-md mt-2">Postman</h4>
         </div>
-        <div className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40" style={{ opacity: 1, transform: 'none' }}>
+        <div className="py-4 px-6 bg-gray-100 m-4 md:w-48 w-40 rounded-lg flex flex-col items-center hover:scale-125 cursor-pointer">
           <img alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" className="w-12" />
-          <h4 className="text-md ml-4">Dart</h4>
+          <h4 className="text-md mt-2">Dart</h4>
         </div>
-        <div className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40" style={{ opacity: 1, transform: 'none' }}>
+        <div className="py-4 px-6 bg-gray-100 m-4 md:w-48 w-40 rounded-lg flex flex-col items-center hover:scale-125 cursor-pointer">
           <img alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" className="w-12" />
-          <h4 className="text-md ml-4">Flutter</h4>
+          <h4 className="text-md mt-2">Flutter</h4>
         </div>
       </div>
     </div>
